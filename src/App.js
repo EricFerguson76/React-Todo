2cb3ec618b33190a5215f41c016c1de5a302af5e
EricFerguson76 <turtle0888@gmail.com>
2019-12-02 18:12:21 -0600

    Add data to App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
 import React from 'react';
 
+const todos = [
+  {
+    name: 'Wash Dishes',
+    id: 1121,
+    purchased: false
+  },
+
+  {
+    name: 'Wash Car',
+    id: 1122,
+    purchased: false
+  },
+
+  {
+    name: 'Vacuum Bedrooms',
+    id: 1123,
+    purchased: false
+  },
+
+  {
+    name: 'Hang Christmas Lights',
+    id: 1124,
+    purchased: false
+  },
+
+  {
+    name: 'Buy Presents',
+    id: 1125,
+    purchased: false
+  },
+
+  {
+    name: 'Decorate Christmas Tree',
+    id: 1126,
+    purchased: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -7,8 +45,22 @@ class App extends React.Component {
 
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      newTodo: ''
+    };
   }
+
+  handleChanges = e => {
+    this.setState({ newTodo: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addTodo(this.state.newTodo);
+    this.setState({
+      newTodo: ''
+    });
+  };
   render() {
     return (
       <div>
